b59592c85372e3cd1390fdc3320109db70bbf06f
ZeGuilhermeYung <jgryung@gmail.com>
2022-08-30 14:28:15 -0300

    just sending and posting tweets

--- index.js
+++ index.js
@@ -6,15 +6,26 @@ app.use(cors());
 app.use(express.json());
 
 const users = [];
+const tweets = [];
 
 app.post('/sign-up', (req, res) => {
   const user = req.body;
   users.push(user);
-  res.send(user);
+  res.send(console.log("OK"));
 });
 
 app.get('/sign-up', (req, res) => {
   res.send(users);
+});
+
+app.post('/tweets', (req, res) => {
+  const tweet = req.body;
+  tweets.push(tweet);
+  res.send(console.log("OK"));
+});
+
+app.get('/tweets', (req, res) => {
+  res.send(tweets);
 });
 
 app.listen(5000);
