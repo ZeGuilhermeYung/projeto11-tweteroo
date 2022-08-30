143cbbe3b85ff83e4c182db348fc3f72ff65d266
ZeGuilhermeYung <jgryung@gmail.com>
2022-08-30 14:05:37 -0300

    start post e get tests

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+const user = {};
+
+app.post('/sign-up', (req, res) => {
+  user = req.body;
+  res.send(user);
+});
+
+app.get('/sign-up', (req, res) => {
+  res.send(user);
+});
+
+app.listen(5000);
